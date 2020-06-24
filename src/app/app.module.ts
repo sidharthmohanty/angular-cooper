2b2da2a41938e669f4cf9b844a4b96f26b71b099
Sidharth Mohanty <sidharthmohanty21@gmail.com>
2020-06-24 23:15:30 +0530

    24 june home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,5 +27,6 @@ import { MatListModule } from '@angular/material/list';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
