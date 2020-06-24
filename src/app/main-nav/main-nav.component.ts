3d3c8e5717f7ddb62fb3925a6648356c7f86cacd
SIDHARTH MOHANTY <sidharth_mohanty@tantra-laptop-nte-999.ga-430.nettantra.com>
2020-06-24 17:51:17 +0530

    june 24 office

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -10,6 +10,14 @@ import { map, shareReplay } from 'rxjs/operators';
   encapsulation: ViewEncapsulation.None,
 })
 export class MainNavComponent {
+  rightIcon = 'd-none';
+  block = 'd-block';
+  hide = 'd-none';
+  toggleDisplay =
+    'col-md-2 col-sm-2 col-lg-2 d-block bg-light sidebar collapse navbar-collapse';
+  toggleHide =
+    'col-md-2 bg-light col-md-2 col-sm-2 col-lg-2 d-none sidebar collapse navbar-collapse';
+  toggleClass = this.toggleDisplay;
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
@@ -18,4 +26,14 @@ export class MainNavComponent {
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  toggle() {
+    if (this.toggleClass === this.toggleDisplay) {
+      this.toggleClass = this.toggleHide;
+      this.rightIcon = this.block;
+    } else {
+      this.toggleClass = this.toggleDisplay;
+      this.rightIcon = this.hide;
+    }
+  }
 }
