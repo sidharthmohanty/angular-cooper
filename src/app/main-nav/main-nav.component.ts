742007c81fb059503c643742d2a10710f69b53f6
Sidharth Mohanty <sidharthmohanty21@gmail.com>
2020-06-28 12:26:25 +0530

    28 june home 1st C

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -4,7 +4,6 @@ import {
   OnInit,
   HostListener,
 } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -15,53 +14,54 @@ import { map, shareReplay } from 'rxjs/operators';
   encapsulation: ViewEncapsulation.None,
 })
 export class MainNavComponent implements OnInit {
+  mainWrapper;
   rightIcon;
   sideIcon;
   toggleSide;
+  wrapper = 'wrapper';
+  collapseWrapper = 'wrapper lapse';
   toggleIn = 'toggle-in';
   toggleOut = 'toggle-out';
-  hideSideIcon = 'd-none';
-  showSideIcon = 'ml-2 pt-light side-label';
-  block = 'd-block';
-  hide = 'd-none';
 
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
   public screenWidth: any;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() {}
   ngOnInit() {
     this.screenWidth = window.innerWidth;
-    if (this.screenWidth < 550) {
-      this.sideIcon = this.hideSideIcon;
+    if (this.screenWidth < 650) {
+      this.mainWrapper = this.collapseWrapper;
       this.toggleSide = this.toggleOut;
     } else {
-      this.sideIcon = this.showSideIcon;
+      this.mainWrapper = this.wrapper;
       this.toggleSide = this.toggleIn;
     }
   }
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.screenWidth = window.innerWidth;
-    if (this.screenWidth < 550) {
-      this.sideIcon = this.hideSideIcon;
+    if (this.screenWidth < 650) {
+      this.mainWrapper = this.collapseWrapper;
       this.toggleSide = this.toggleOut;
     } else {
-      this.sideIcon = this.showSideIcon;
+      this.mainWrapper = this.wrapper;
       this.toggleSide = this.toggleIn;
     }
   }
   toggle() {
-    if (this.sideIcon === this.showSideIcon) {
-      this.sideIcon = this.hideSideIcon;
+    if (this.mainWrapper === this.wrapper) {
+      this.mainWrapper = this.collapseWrapper;
       this.toggleSide = this.toggleOut;
     } else {
-      this.sideIcon = this.showSideIcon;
+      this.mainWrapper = this.wrapper;
       this.toggleSide = this.toggleIn;
     }
+    // if (this.sideIcon === this.showSideIcon) {
+
+    //   this.sideIcon = this.hideSideIcon;
+    //   this.toggleSide = this.toggleOut;
+    // } else {
+    //   this.sideIcon = this.showSideIcon;
+    //   this.toggleSide = this.toggleIn;
+    // }
   }
 }
